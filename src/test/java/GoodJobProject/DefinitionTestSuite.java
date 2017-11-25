b995c6dac80496a14d77d5db409c3ca6a8261fde
truyenkv <kieuvantruyen.3dfish@gmail.com>
2017-11-25 10:45:23 +0700

    1

---
+++ src/test/java/GoodJobProject/DefinitionTestSuite.java
@@ -0,0 +1,9 @@
+package GoodJobProject;
+
+import cucumber.api.CucumberOptions;
+import net.serenitybdd.cucumber.CucumberWithSerenity;
+import org.junit.runner.RunWith;
+
+@RunWith(CucumberWithSerenity.class)
+@CucumberOptions(features="src/test/resources/features/LookupADefinition.feature")
+public class DefinitionTestSuite {}
